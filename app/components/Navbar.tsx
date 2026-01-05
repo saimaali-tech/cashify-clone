c490fdf97abdcfd57b313213b0e671775bd821bc
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-05 14:49:45 +0500

    changed the padding of searchbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -25,7 +25,7 @@ const Search = styled('div')(({ theme }) => ({
 }));
 
 const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
+  padding: theme.spacing(0, 3),
   height: '100%',
   position: 'absolute',
   pointerEvents: 'none',
@@ -37,13 +37,13 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
+    padding: theme.spacing(1, 2, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create('width'),
-    width: '100%',
+    width: '70%',
     [theme.breakpoints.up('md')]: {
-      width: '20ch',
+      width: '40ch',
     },
   },
 }));
