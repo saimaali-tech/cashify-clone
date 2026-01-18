dac97c5250ab51bb4d382275892d97a358cf751f
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-18 15:41:35 +0500

    imported carousel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/Navbar/navbar";
+import Carousel from "./components/Carousel";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
+        <Carousel/>
         
         {children}
       </body>
