d4aec9ffbcc37a63e4f88a214956df82a36a0283
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-18 16:13:09 +0500

    applied css in global css

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,6 +2,7 @@
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -9,33 +10,29 @@ import "swiper/css/pagination";
 
 export default function Carousel() {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      spaceBetween={30}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 3000 }}
-      loop={true}
-        
-    >
-      <SwiperSlide>
-       <img src="/docker.png" />
-      </SwiperSlide>
-
-      <SwiperSlide>
-      <img src="/aws2.jpeg" />
-      </SwiperSlide>
-
-   
-       <SwiperSlide>
-       <img src="/docker.png" />
-      </SwiperSlide>
-
-      <SwiperSlide>
-      <img src="/aws2.jpeg" />
-      </SwiperSlide>
-
-    </Swiper>
+    <div className="carousel-container">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop
+        centeredSlides={true}
+      >
+        {["aws.jpeg", "docker.png", "aws2.jpeg"].map((img, i) => (
+          <SwiperSlide key={i}>
+            <div className="slide">
+              <Image
+                src={`/${img}`}
+                alt="slide"
+                fill
+                style={{ objectFit: "contain" }}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 }
