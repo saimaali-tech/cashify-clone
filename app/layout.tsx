5582a931af6eb7c93fa24038c3fdfcd25e976aec
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-18 16:15:36 +0500

    applied css in global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
-      
-        
+        <Navbar /> 
         {children}
       </body>
     </html>
