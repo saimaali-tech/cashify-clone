070163e5f9e483bad01566e95d3d6918391b946f
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-24 23:06:36 +0500

    import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Carousel from './components/Carousel';
-import 
+import  Services from './components/Services'
 
 export default function Home() {
   return (
     <main className="page-center">
     <Carousel/>
+    <Services />
     </main>
   );
 }
