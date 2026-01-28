b54ca2e0e053d5476f1b09ec28e3c272eae11162
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-28 21:04:55 +0500

    imported exclusivestore.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import  Services from './components/Services'
 import  Sell from './components/Sell'
 import Refirbished from './components/Refirbished'
 import RefurbishedLaptops from './components/RefurbishedLaptops'
+import ExclusiveStores from './components/ExclusiveStores';
 
 
 
@@ -14,6 +15,7 @@ export default function Home() {
     <Sell/>
     <Refirbished/>
     <RefurbishedLaptops/>
+    <ExclusiveStores/>
    
     </main>
   );
