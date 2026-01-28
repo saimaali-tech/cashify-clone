5fffdffc245ab4e250a737cba4a077d92f652a3f
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-28 20:58:15 +0500

    added content for  RefurbishedLaptops

--- app/components/RefurbishedLaptops.tsx
+++ app/components/RefurbishedLaptops.tsx
@@ -49,7 +49,7 @@ const RefurbishedLaptops = () => {
   ];
 
   return (
-    <div className="box">
+    <div className="laptop">
       <h1>Refurbished Laptops</h1>
       <div className="laptop-grid">
         {laptops.map((laptop, index) => (
