1f593a6948fac9639250088bb5e9992aecdd9ac7
saimaali-tech <saimaalisaima313@gmail.com>
2026-01-29 13:05:12 +0500

    chnaged size of images

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Carousel from './components/Carousel';
 import  Services from './components/Services'
 import  Sell from './components/Sell'
 import Refirbished from './components/Refirbished'
-import RefurbishedLaptops from './components/RefurbishedLaptops'
+// import RefurbishedLaptops from './components/RefurbishedLaptops'
 
 
 
@@ -14,7 +14,7 @@ export default function Home() {
     <Services />
     <Sell/>
     <Refirbished/>
-    <RefurbishedLaptops/>
+    {/* <RefurbishedLaptops/> */}
    
    
     </main>
