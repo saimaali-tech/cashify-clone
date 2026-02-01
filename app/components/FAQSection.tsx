67f693deb1460497c34be49e1908295e93651b2b
saimaali-tech <saimaalisaima313@gmail.com>
2026-02-01 23:12:20 +0500

    changed the content of FAQSection

--- app/components/FAQSection.tsx
+++ app/components/FAQSection.tsx
@@ -43,7 +43,7 @@ const FAQSection: React.FC = () => {
 
   return (
     <Box sx={{ p: { xs: 2, md: 4 }, bgcolor: 'white' }}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 3 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', mb: 3 }}>
         <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
           Frequently Asked Questions
         </Typography>
