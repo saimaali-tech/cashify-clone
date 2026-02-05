b3d3ba3e884dbad0857fd6b37ca678d0a0463d7e
saimaali-tech <saimaalisaima313@gmail.com>
2026-02-05 15:53:36 +0500

    changed the images

--- app/components/TrendingArticlesSection.tsx
+++ app/components/TrendingArticlesSection.tsx
@@ -11,21 +11,21 @@ import {
 
 const trendingArticles = [
   {
-    image: '/images/Samsung-Galaxy.jpg', // For Moto G77
+    image: '/images/design-2026-02-03T10225.jpg', // For Moto G77
     title: 'Moto G77 Camera Specs, Battery, and Performance: See All Details Here',
     description:
       'Moto G77. Yes, finally, Motorola has just dropped a camera maestro! Yes, finally, Motorola has launched a great camera value for the budget segment. Motorola is coming up with the new Moto G77. The phone got announced this week, and in just two days, the whole social media community started talking about this phone. This phone is among the...',
     date: '30th Jan 2026',
   },
   {
-    image: '/images/Samsung-Galaxy (1).jpg',
+    image: '/images/design-2026-02-03T13242.jpg',
     title: 'Samsung S26 Ultra Smart Localized Privacy Screen: Here’s How It Works',
     description:
       'Using your phone in public always come with a small risk. Whether you’re checking messages in a crowded metro, replying to a work email in a cafe, or opening a banking app while standing in line, your screen is visible to people around you. Until now, smartphone privacy features used to...',
     date: '30th Jan 2026',
   },
   {
-    image: '/images/Realme-P4.jpg',
+    image: '/images/design-2026-02-04T18252.jpg',
     title: 'Realme P4 Power Vs Vivo T4: Price Difference, Specs, All Details!',
     description:
       'I’ve been checking out some newly released smartphones, the Realme P4 Power Vs Vivo T4 are standing out! They are great and it’s confusing to choose between them. I found that both of them are crazy powerful for mid-range phones. The Realme P4 Power is great if you care about long use, because it...',
